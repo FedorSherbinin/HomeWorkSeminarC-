56913da35d25d9012dcdfa7b95d8e8bbf77322a9
FedorSherbinin <137335853+FedorSherbinin@users.noreply.github.com>
2023-07-08 20:52:56 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -21,3 +21,24 @@ int num = Convert.ToInt32(Console.ReadLine());
     else
     Console.WriteLine("it's not a three digit number: " );
 */    
+
+// Task 2. Напишите программу, которая выводит третью цифру заданного числа.
+
+int ThirdDigit(int num)
+{
+    int Number = num % 1000;
+        Number = Number / 100;
+    
+    int result = Number;
+    return result;
+}
+
+Console.WriteLine("Imput a Number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+    if(num >=100 )
+    {
+        int newNumber = ThirdDigit(num);
+        Console.WriteLine(newNumber);
+    }
+    else
+    Console.WriteLine("there is no third digit");
