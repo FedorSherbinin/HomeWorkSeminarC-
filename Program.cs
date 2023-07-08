5eb46c54c090f3458d5ccbb087b6f53ce807f1c5
FedorSherbinin <fredersherb@gmail.com>
2023-07-08 21:04:39 +0300

    Task 2

--- Program.cs
+++ Program.cs
@@ -1,27 +1,5 @@
-﻿/*
-// Task 1. Напишите программу, которая принимает на вход
-  // трёхзначное число и на выходе показывает вторую цифру этого числа.
-
-int randomNumber(int num)
-{
-    int Number = num % 100;
-        Number = Number / 10;
-
-    int result = Number;
-    return result;
-}
-
-Console.WriteLine("Imput a Triple Digits: " );
-int num = Convert.ToInt32(Console.ReadLine());
-    if(num >=100 && num < 1000)
-    {
-        int newNumber = randomNumber(num);
-        Console.WriteLine(newNumber);
-    }
-    else
-    Console.WriteLine("it's not a three digit number: " );
-*/    
-
+﻿
+/*-
 // Task 2. Напишите программу, которая выводит третью цифру заданного числа.
 
 int ThirdDigit(int num)
@@ -42,3 +20,4 @@ int num = Convert.ToInt32(Console.ReadLine());
     }
     else
     Console.WriteLine("there is no third digit");
+*/
