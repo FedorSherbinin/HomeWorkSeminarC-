f8f446ceb93b63b0895b50c3a7bfbe6962dd0993
FedorSherbinin <137335853+FedorSherbinin@users.noreply.github.com>
2023-07-08 20:07:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+// Task 1. Напишите программу, которая принимает на вход
+  // трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int randomNumber(int num)
+{
+    int Number = num % 100;
+        Number = Number / 10;
+
+    int result = Number;
+    return result;
+}
+
+Console.WriteLine("Imput a Triple Digits: " );
+int num = Convert.ToInt32(Console.ReadLine());
+    if(num >=100 && num < 1000)
+    {
+        int newNumber = randomNumber(num);
+        Console.WriteLine(newNumber);
+    }
+    else
+    Console.WriteLine("it's not a three digit number: " );
+*/    
